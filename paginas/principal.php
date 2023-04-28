4b46651e9b2d43bfaa45a3eaaa952f316a2ce775
edutbx <eduardo.abreu8@etec.sp.gov.br>
2023-04-28 15:38:01 -0300

    o igor ta me apressando e subi assim mesmo

--- paginas/principal.php
+++ paginas/principal.php
@@ -6,29 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos/stylep.css">
-    <!-- <script>
-        var slideIndex = 1;
-        showSlides(slideIndex);
-
-        function plusSlides(n) {
-            showSlides(slideIndex += n);
-        }
-
-        function showSlides(n) {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            if (n > slides.length) {
-                slideIndex = 1;
-            }
-            if (n < 1) {
-                slideIndex = slides.length;
-            }
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slides[slideIndex - 1].style.display = "block";
-        }
-    </script> -->
     <title>Document</title>
 </head>
 
@@ -44,24 +21,6 @@
 
     fjsdafhsghsighsdfg
     </main>
-
-
-    <!-- <div class="slideshow-container">
-        <div class="mySlides fade">
-            <img src="img/bonecos.JPG" style="width:100px">
-        </div>
-
-        <div class="mySlides fade">
-            <img src="img/logo.svg" style="width:100%">
-        </div>
-
-        <div class="mySlides fade">
-            <img src="img3.jpg" style="width:100%">
-        </div>
-
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-    </div> -->
 </body>
 
 </html>
