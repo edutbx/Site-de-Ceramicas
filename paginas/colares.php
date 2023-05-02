577aa220bf3ec6f2465f4bdae12944f2bb1c692a
IgaoIG <igor.oliveira234@etec.sp.gov.br>
2023-05-02 16:59:33 -0300

    mudancas de erros é os cria

--- paginas/colares.php
+++ paginas/colares.php
@@ -1,47 +1,40 @@
-<style>
-    .foto:hover{
-        box-shadow: 0px 0px 100px red;
-        rotate: 360deg;
-    }
-
-    .foto{
-        transition: 0.5s;
-    }
-</style>
-
 <main>
     <div class="mains">
-            <section class="foto">
-            <img src="img/colar1.jpg" alt="colar1" width="200px" height="220px">
-            <p>teste</p>
-            </section>
-            <section class="foto">
-            <img src="img/colar2.jpg" alt="colar2" width="200px" height="220px">
-            <p>teste</p>
-            </section>
-            <section class="foto">
-            <img src="img/colar3.jpg" alt="colar3" width="200px" height="220px">
-            <p>teste</p>
-            </section>
-            <section class="foto">
-            <img src="img/bonecos.jpg" alt="bonecos" width="200px" height="220px">
-            <p>teste</p>
-            </section>
-            <section class="foto">
-            <img src="img/bonecos.jpg" alt="bonecos" width="200px" height="220px">
-            <p>teste</p>
-            </section>
-            <section class="foto">
-            <img src="img/bonecos.jpg" alt="bonecos" width="200px" height="220px">
-            <p>teste</p>
-            </section>
-            <section class="foto">
-            <img src="img/bonecos.jpg" alt="bonecos" width="200px" height="220px">
-            <p>teste</p>
-            </section>
-            <section class="foto">
-            <img src="img/bonecos.jpg" alt="bonecos" width="200px" height="220px">
-            <p>teste</p>
-            </section>
+            <div class="row">
+                <section class="foto">
+                <img src="img/colar1.jpg" alt="colar1" width="200px" height="220px">
+                <p>teste</p>
+                </section>
+                <section class="foto">
+                <img src="img/colar2.jpg" alt="colar2" width="200px" height="220px">
+                <p>teste</p>
+                </section>
+                <section class="foto">
+                <img src="img/colar3.jpg" alt="colar3" width="200px" height="220px">
+                <p>teste</p>
+                </section>
+                <section class="foto">
+                <img src="img/bonecos.jpg" alt="bonecos" width="200px" height="220px">
+                <p>teste</p>
+                </section>
+            </div>
+            <div class="row">
+                <section class="foto">
+                <img src="img/colar1.jpg" alt="colar1" width="200px" height="220px">
+                <p>teste</p>
+                </section>
+                <section class="foto">
+                <img src="img/colar2.jpg" alt="colar2" width="200px" height="220px">
+                <p>teste</p>
+                </section>
+                <section class="foto">
+                <img src="img/colar3.jpg" alt="colar3" width="200px" height="220px">
+                <p>teste</p>
+                </section>
+                <section class="foto">
+                <img src="img/bonecos.jpg" alt="bonecos" width="200px" height="220px">
+                <p>teste</p>
+                </section>
+            </div>
     </div>
 </main>
