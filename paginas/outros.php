362b279ea74db90c1162fceeb8497d6a04ccef61
edutbx <eduardo.abreu8@etec.sp.gov.br>
2023-05-03 00:25:01 -0300

    Adicionei varias foto daora

--- paginas/outros.php
+++ paginas/outros.php
@@ -2,37 +2,37 @@
     <div class="mains">
             <div class="row">
                 <section class="foto">
-                <img src="img/colar1.jpg" alt="colar1" width="200px" height="220px">
+                <img src="img/outros1.jpg" alt="outros1" width="200px" height="220px">
                 <p>teste</p>
                 </section>
                 <section class="foto">
-                <img src="img/colar2.jpg" alt="colar2" width="200px" height="220px">
+                <img src="img/outros2.jpg" alt="outros2" width="200px" height="220px">
                 <p>teste</p>
                 </section>
                 <section class="foto">
-                <img src="img/colar3.jpg" alt="colar3" width="200px" height="220px">
+                <img src="img/outros3.jpg" alt="outros3" width="200px" height="220px">
                 <p>teste</p>
                 </section>
                 <section class="foto">
-                <img src="img/bonecos.jpg" alt="bonecos" width="200px" height="220px">
+                <img src="img/outros4.jpg" alt="outros4" width="200px" height="220px">
                 <p>teste</p>
                 </section>
             </div>
             <div class="row">
                 <section class="foto">
-                <img src="img/colar1.jpg" alt="colar1" width="200px" height="220px">
+                <img src="img/outros5.jpg" alt="outros5" width="200px" height="220px">
                 <p>teste</p>
                 </section>
                 <section class="foto">
-                <img src="img/colar2.jpg" alt="colar2" width="200px" height="220px">
+                <img src="img/outros6.jpg" alt="outros6" width="200px" height="220px">
                 <p>teste</p>
                 </section>
                 <section class="foto">
-                <img src="img/colar3.jpg" alt="colar3" width="200px" height="220px">
+                <img src="img/outros7.jpg" alt="outros7" width="200px" height="220px">
                 <p>teste</p>
                 </section>
                 <section class="foto">
-                <img src="img/bonecos.jpg" alt="bonecos" width="200px" height="220px">
+                <img src="img/outros8.jpg" alt="outros8" width="200px" height="220px">
                 <p>teste</p>
                 </section>
             </div>
