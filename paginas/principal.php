be456e814b2fbb5ce3ada731d1071f2704491bab
IgaoIGz <iguel.oliveira@gmail.com>
2023-05-03 03:32:16 -0300

    algumas coisas ae

--- paginas/principal.php
+++ paginas/principal.php
@@ -11,14 +11,38 @@
 
 <body>
     <main>
-        <div class="container">
-            <div class="div1">
-                <p>mensagem super daora aqui</p>
-            </div>
-            <img src="img/copos.JPG" alt="copos" width="44%">
+        <div class="banner">
+            <img src="img/bonecos.jpg" alt="" height="300px" class="image">
+            <img src="img/svg/de-coracao.svg" alt="de-coracao" width="650px">
         </div>
 
-        AAAAAAAAAA
+        <div class="containerp">
+            <h1>
+                "Amor a arte em cerâmica"
+            </h1>
+            <p>
+                Possuímos uma variedade de produtos cujo material principal é... cerâmica!
+            </p>
+        </div>
+
+        <div class="container">
+            <div class="div1">
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia porro provident tenetur odio minus blanditiis ea, non asperiores delectus corporis doloremque doloribus. Ipsa dicta, non alias explicabo repudiandae excepturi commodi.
+                </p>
+            </div>
+            <img src="img/copos.JPG" alt="copos" width="44%" class="image">
+        </div>
+
+        <div class="cirilo">
+            <div class="produtos">
+                <div class="produto">p1</div>
+                <div class="produto">p2</div>
+                <div class="produto">p3</div>
+                <div class="produto">p4</div>
+            </div>
+            <button>ver mais</button>
+        </div>
     </main>
 </body>
 
