bcd399d53d18dd8c0f1c309f585f4a5b9f27d4c6
edutbx <eduardo.abreu8@etec.sp.gov.br>
2023-05-07 00:15:49 -0300

    coloquei coisa na pag criadora

--- paginas/cria.php
+++ paginas/cria.php
@@ -1,1 +1,79 @@
-    Uma mulher apaixonada por cerâmicas que ama o que faz. Possui anos de experiência e resolveu espalhar a sua arte e transimitir essa paixão incrível.
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    .div-header {
+        width: 100%;
+        height: 110px;
+    }
+
+    .main-div {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        text-align: center;
+        font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+        width: 100%;
+        height: 600px;
+        margin-bottom: 40px;
+        font-weight: bold;
+        text-align: justify;
+    }
+
+    .div1 {
+        font-size: 16px;
+        border-radius: 10px;
+        width: 40%;
+        transition: 1s;
+        background: linear-gradient(to bottom right, #A3C6B3, #E7D2AD);
+        border-radius: 10px;
+        padding: 20px;
+        box-shadow: 5px 5px 5px gray;
+        
+    }
+
+    .div1:hover {
+        box-shadow: none;
+    }
+
+    .div2 {
+        background-color: green;
+    }
+
+    h1 {
+        text-align: center;
+        padding-top: 40px;
+    }
+</style>
+<div class="div-header"></div>
+<h1>
+    <p style="color: white;">CRIADORA</p>
+</h1>
+<div class="main-div">
+    <div class="div1">
+        <p>
+            Conheça a história de uma mulher talentosa e apaixonada por arte. Ela descobriu sua paixão por fazer bijuterias de cerâmica há alguns anos e desde então tem se dedicado incansavelmente a aprimorar suas habilidades e criar peças únicas e deslumbrantes.
+        </p><br>
+
+        <p>
+            Cada peça que ela cria é um reflexo da sua personalidade, do seu amor pela arte e da sua dedicação em criar algo verdadeiramente especial. Cada detalhe é cuidadosamente pensado e trabalhado para garantir que suas bijuterias sejam verdadeiras obras de arte.
+        </p><br>
+
+
+        <p>
+            Além de criar essas peças incríveis, ela também as vende. E cada vez que uma de suas bijuterias encontra um novo lar, ela fica emocionada por saber que alguém irá usá-la e apreciá-la tanto quanto ela. Para ela, é uma honra saber que suas criações estão sendo usadas para trazer felicidade e estilo à vida das pessoas.
+        </p><br>
+
+        <p>
+            Não há nada mais gratificante do que seguir sua paixão e ver os frutos do seu trabalho. E é exatamente isso que essa mulher está fazendo. Ela está seguindo sua paixão, criando arte e trazendo beleza e felicidade para as pessoas. Não é apenas sobre as bijuterias, é sobre a paixão, dedicação e amor que ela coloca em cada peça. É sobre a história e a personalidade que cada bijuteria carrega consigo.
+        </p><br>
+
+
+        <p>
+            Se você ainda não teve a oportunidade de ver o trabalho incrível dessa mulher, não perca mais tempo. Venha conhecer suas criações e deixe-se encantar pela sua história e sua arte. Você não irá se arrepender."
+        </p><br>
+    </div>
+</div>
